7a9837857ffde2c1a0ea670bfeda933165df289d
jonathan-oberlander <jonathan.oberlander@gmail.com>
2025-08-27 21:36:24 +0100

    Use options and Results + add handlers + readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Todo app written in OCaml
+
+Terminal app. Dune project to learn OCaml.
+
+``` bash
+--- To-Do List Menu ---
+1. List Tasks
+2. Add Task
+3. Update Task
+4. Complete Task
+5. Delete Task
+6. Quit
+Choose an option: 1
+1. [x] pet the cat
+2. [ ] pet the dog
+```
--- bin/main.ml
+++ bin/main.ml
@@ -3,6 +3,10 @@ type task =
   ; completed : bool
   }
 
+type error = IndexOutOfBounds of int
+
+(* ---------------------------------------------------------------- *)
+
 let todo_list = List.[]
 
 let print_task index task =
@@ -16,44 +20,115 @@ let print_all_tasks todo_list =
 
 let add_task tasks description =
   let new_task = { description; completed = false } in
-  List.cons new_task tasks
+  Ok (new_task :: tasks)
 ;;
 
-let delete_task tasks idx = List.filteri (fun i _ -> i <> idx) tasks
+let update_task tasks idx description =
+  if idx < 0 || idx >= List.length tasks
+  then Error (IndexOutOfBounds idx)
+  else
+    Ok
+      (List.mapi
+         (fun i task -> if i = idx then { task with description } else task)
+         tasks)
+;;
+
+let delete_task tasks idx =
+  if idx < 0 || idx >= List.length tasks
+  then Error (IndexOutOfBounds idx)
+  else Ok (List.filteri (fun i _ -> i <> idx) tasks)
+;;
 
 let mark_completed tasks idx =
-  List.mapi (fun i task -> if i = idx then { task with completed = true } else task) tasks
+  if idx < 0 || idx >= List.length tasks
+  then Error (IndexOutOfBounds idx)
+  else
+    Ok
+      (List.mapi
+         (fun i task -> if i = idx then { task with completed = true } else task)
+         tasks)
 ;;
+
+(* ---------------------------------------------------------------- *)
+
+let add_task_handler todo_list =
+  print_endline "Enter task description:";
+  let desc = read_line () in
+  match add_task todo_list desc with
+  | Ok list -> list
+  | Error _ ->
+    print_endline "Failed to add task.";
+    todo_list
+;;
+
+let print_all_tasks_handler todo_list =
+  print_all_tasks todo_list;
+  todo_list
+;;
+
+let complete_task_handler todo_list =
+  print_string "Enter task number to mark as complete (starting from 1): ";
+  match int_of_string_opt (read_line ()) with
+  | None ->
+    print_endline "Please enter a valid number.";
+    todo_list
+  | Some num ->
+    (match mark_completed todo_list (num - 1) with
+     | Ok list -> list
+     | Error (IndexOutOfBounds n) ->
+       Printf.printf "Failed to complete task %d. Is the task index correct?\n" n;
+       todo_list)
+;;
+
+let update_task_handler todo_list =
+  print_string "Enter task number to update (starting from 1): ";
+  match int_of_string_opt (read_line ()) with
+  | None ->
+    print_endline "Please enter a valid number.";
+    todo_list
+  | Some num ->
+    print_string "Enter new description: ";
+    let desc = read_line () in
+    (match update_task todo_list (num - 1) desc with
+     | Ok updated -> updated
+     | Error (IndexOutOfBounds n) ->
+       Printf.printf "Invalid task number: %d." n;
+       todo_list)
+;;
+
+let delete_task_handler todo_list =
+  print_string "Enter task number to delete (startin from 1): ";
+  match int_of_string_opt (read_line ()) with
+  | None ->
+    print_endline "Please enter a valid number.";
+    todo_list
+  | Some num ->
+    (match delete_task todo_list (num - 1) with
+     | Ok list -> list
+     | Error (IndexOutOfBounds n) ->
+       Printf.printf "Failed to delete task %d. Is the task index correct?\n" n;
+       todo_list)
+;;
+
+(* ---------------------------------------------------------------- *)
 
 let rec main_loop todo_list =
   print_endline "\n--- To-Do List Menu ---";
-  print_endline "1. Add Task";
-  print_endline "2. List Tasks";
-  print_endline "3. Complete Task";
-  print_endline "4. Delete Task";
-  print_endline "5. Quit";
+  print_endline "1. List Tasks";
+  print_endline "2. Add Task";
+  print_endline "3. Update Task";
+  print_endline "4. Complete Task";
+  print_endline "5. Delete Task";
+  print_endline "6. Quit";
   print_string "Choose an option: ";
   let choice = read_line () in
   match choice with
-  | "1" ->
-    print_string "Enter task description: ";
-    let desc = read_line () in
-    let todo_list = add_task todo_list desc in
-    main_loop todo_list
-  | "2" ->
-    print_all_tasks todo_list;
-    main_loop todo_list
-  | "3" ->
-    print_string "Enter task number to mark as complete (starting from 1): ";
-    let num = int_of_string (read_line ()) in
-    let todo_list = mark_completed todo_list (num - 1) in
-    main_loop todo_list
-  | "4" ->
-    print_string "Enter task number to delete (startin from 1): ";
-    let num = int_of_string (read_line ()) in
-    let todo_list = delete_task todo_list (num - 1) in
-    main_loop todo_list
-  | "5" -> print_endline "Goodbye!"
+  | "1" -> main_loop (print_all_tasks_handler todo_list)
+  | "2" -> main_loop (add_task_handler todo_list)
+  | "3" -> main_loop (update_task_handler todo_list)
+  | "4" -> main_loop (complete_task_handler todo_list)
+  | "5" -> main_loop (delete_task_handler todo_list)
+  | "6" -> print_endline "Goodbye!"
   | _ ->
     print_endline "Invalid option.";
     main_loop todo_list
